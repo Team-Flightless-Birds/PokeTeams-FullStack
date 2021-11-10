364013ff8eaac62268f6c3deb186d59b1eff1e2f
David Xue <xuegdxw@gmail.com>
2021-11-10 13:46:48 -0500

    Added Deploymen instructions to README & Added DBInitializer.php

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # PokeTeams-React
-Front end for PokeTeams project for CS 4750: Database Systems at the University of Virginia
+PokeTeams is a full stack CRUD project for CS 4750: Database Systems course at the University of Virginia
+
+# Deployment
+## Deploying React (frontend) Project 
+1. Always ensure you have done ```git pull origin main``` before you do any commits to avoid merge conflicts.
+1. Ensure changes work locally on your local React server.
+    - ```npm start```
+2. Ensure react project can build locally
+    - ```npm run build```
+3. Make git commits to main branch or commit to feature branch then merge
+    - ```git add ...```
+    - ```git commit ...```
+    - ```git push origin main```
+    - Note: You do not need to commit the build folder or the node_modules folder, they are automatically ignored in .gitignore so they won't be committed.
+4. A successful commit or merge to main will trigger the following:
+    - Github NodeJS CI workflow will attempt to build to ensure build is successful
+    - Then, Google Cloud Build Continueous Deployment will build the build folder, and upload/deploy the project to the GCP. Note: In this process, both the backend PHP and frontend React will be built and deployed even if one or the other did not have any changes.
+- Note: No manual deployment to frontend (such as running ``` gcloud app deploy frontend/frontend.yaml``` ) should be necessary unless for debugging purposes.
+
+## Deploying to PHP (backend) 
+1. Always ensure you have done ```git pull origin main``` before you do any commits to avoid merge conflicts.
+1. Make necessary changes to the backend contents (in the backend folder) and push the commit to main branch.
+2. A successful commit or merge to main will trigger the following:
+    - Github NodeJS CI workflow will attempt to build the react project to ensure build is successful
+    - Then, Google Cloud Build Continueous Deployment will build the NodeJS build folder, and upload/deploy the project to the GCP. Note: In this process, both the backend PHP and frontend React will be built and deployed even if one or the other did not have any changes.
+- Note: No manual deployment to the backend (such as running ``` gcloud app deploy backend/backend.yaml``` ) should be necessary unless for debugging purposes.
---
+++ backend/DBInitializer.php
@@ -0,0 +1,76 @@
+<?php
+declare(strict_types=1);
+
+namespace Google\Cloud\Samples\CloudSQL\MySQL;
+
+use PDO;
+use PDOException;
+use RuntimeException;
+
+class DBInitializer
+{
+    /**
+     *  @param $username string username of the database user
+     *  @param $password string password of the database user
+     *  @param $dbName string name of the target database
+     *  @param $connectionName string Cloud SQL instance name
+     *  @param $socketDir string Full path to unix socket
+     *  @param $conn_config array driver-specific options for PDO
+     */
+    public static function initUnixDatabaseConnection(
+        string $username,
+        string $password,
+        string $dbName,
+        string $connectionName,
+        string $socketDir,
+        array $conn_config
+    ): PDO {
+        try {
+            # [START cloud_sql_mysql_pdo_create_socket]
+            $username = 'root';
+            $password = 'yoursupersecretpassword';
+            // $dbName = 'your_db_name';
+            // $connectionName = getenv("CLOUD_SQL_CONNECTION_NAME");
+            // $socketDir = getenv('DB_SOCKET_DIR') ?: '/cloudsql';
+
+            // Connect using UNIX sockets
+            $dsn = sprintf(
+                'mysql:dbname=%s;unix_socket=%s/%s',
+                $dbName,
+                $socketDir,
+                $connectionName
+            );
+
+            // Connect to the database.
+            $conn = new PDO($dsn, $username, $password, $conn_config);
+            # [END cloud_sql_mysql_pdo_create_socket]
+        } catch (TypeError $e) {
+            throw new RuntimeException(
+                sprintf(
+                    'Invalid or missing configuration! Make sure you have set ' .
+                    '$username, $password, $dbName, and $dbHost (for TCP mode) ' .
+                    'or $connectionName (for UNIX socket mode). ' .
+                    'The PHP error was %s',
+                    $e->getMessage()
+                ),
+                (int) $e->getCode(),
+                $e
+            );
+        } catch (PDOException $e) {
+            throw new RuntimeException(
+                sprintf(
+                    'Could not connect to the Cloud SQL Database. Check that ' .
+                    'your username and password are correct, that the Cloud SQL ' .
+                    'proxy is running, and that the database exists and is ready ' .
+                    'for use. For more assistance, refer to %s. The PDO error was %s',
+                    'https://cloud.google.com/sql/docs/mysql/connect-external-app',
+                    $e->getMessage()
+                ),
+                (int) $e->getCode(),
+                $e
+            );
+        }
+
+        return $conn;
+    }
+}
