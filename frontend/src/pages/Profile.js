687b3017dea8b1fa074dcc8a9f55fb9be7c461cb
QasimAli0 <60446478+QasimAli0@users.noreply.github.com>
2021-11-29 18:13:25 -0500

    small profile.js changes

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,10 +1,20 @@
 import '../App.css';
 import React from "react";
 
-export default function Profile() {
-    return(
-        <div className='App-header'>
-            Teams
-        </div>
-    )
+export default function Profile({auth, handleChange}) {
+
+    if (!auth) {
+        return(
+            <div className='App-header'>
+                Please login to continue, budular
+            </div>
+        )
+    }
+    else {
+        return(
+            <div className='App-header'>
+                You are logged in, bud
+            </div>
+        )
+    }
 }
